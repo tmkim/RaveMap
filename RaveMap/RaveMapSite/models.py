7b7267c1a7dda023a1a165ee5dbfaa5b5f2e8745
tmkim <taeminkim13@gmail.com>
2019-03-11 17:45:16 -0400

    Basic website setup in Django

---
+++ RaveMap/RaveMapSite/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from datetime import datetime
+from django.db import models
+
+# Create your models here.
+class RaveEvent(models.Model):
+    title = models.CharField(max_length=200)
+    artists = models.TextField()
+    location = models.TextField()
+    Date = models.DateTimeField()
+    
