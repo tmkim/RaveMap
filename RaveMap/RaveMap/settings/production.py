f8d84a2aaecf59ebf753153bde02606924665301
tmkim <taeminkim13@gmail.com>
2019-03-25 17:13:27 -0400

    cleanup

--- RaveMap/RaveMap/settings/production.py
+++ RaveMap/RaveMap/settings/production.py
@@ -128,7 +128,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
 
 CORS_REPLACE_HTTPS_REFERER      = True
